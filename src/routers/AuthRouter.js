d24dcb907c548293cc6baddfe9d8d8fe6df03814
DiegoDuran <diego.duran@gmail.com>
2021-04-03 16:46:12 -0300

    hasta ejercicio 218

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -5,16 +5,20 @@ import { RegisterScreen } from '../components/auth/RegisterScreen'
 
 export const AuthRouter = () => {
     return (
-        <Switch>
-            <Route exact path="/auth/login"
-                component={LoginScreen}
-            />
-            <Route
-                exact path='/auth/register'
-                component={RegisterScreen}
-            />
-            {/* cualquier otra ruta que no sea esa.. register */}
-            <Redirect to="/auth/login"/> 
-        </Switch>
+        <div className="auth__main">
+            <div className="auth__box-container">
+                <Switch>
+                    <Route exact path="/auth/login"
+                        component={LoginScreen}
+                    />
+                    <Route
+                        exact path='/auth/register'
+                        component={RegisterScreen}
+                    />
+                    {/* cualquier otra ruta que no sea esa.. register */}
+                    <Redirect to="/auth/login"/> 
+                </Switch>
+            </div>
+        </div>
     )
 }
