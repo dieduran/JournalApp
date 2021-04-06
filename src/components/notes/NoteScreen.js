9bb59baf0d60033f0945beb16883e85ce27330e3
DiegoDuran <diego.duran@gmail.com>
2021-04-05 22:30:53 -0300

    hasta ejercicio 225

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
     return (
         <div className="notes_main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                ></input>
+                <textarea
+                    placeholder="Whats happened today"
+                    className="notes__textarea"
+                ></textarea>
+                <div className="notes__images">
+                    <img
+                        src="https://png.pngtree.com/thumb_back/fh260/back_our/20190617/ourmid/pngtree-holiday-landscape-mountain-river-background-vector-material-image_124327.jpg"
+                        alt="Imagen"
+                    >
+                    </img>
+                </div>
+
+            </div>
         </div>
     )
 }
